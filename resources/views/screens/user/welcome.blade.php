4c4e2037cd48f949e00ea944cb562155cfe37f99
RosoolBux <r.bux@4acetech.com>
2024-04-05 16:20:13 +0500

    exotic services second commit

--- resources/views/screens/user/welcome.blade.php
+++ resources/views/screens/user/welcome.blade.php
@@ -285,6 +285,7 @@
                         <div class="col-md-4 col-sm-6 col-12">
                             <div class="card border border-2">
                                 <div class="card-body mb-5">
+                                    <?xml version="1.0" encoding="UTF-8"?>
                                     <?xml version="1.0" encoding="UTF-8"?><svg width="100px" height="100px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M10 12H5C4.44772 12 4 11.5523 4 11V7.5C4 6.94772 4.44772 6.5 5 6.5H9C9.55228 6.5 10 6.94772 10 7.5V12ZM10 12C10 14.5 9 16 6 17.5" stroke="var(--background-color)" stroke-width="1.5" stroke-linecap="round"></path><path d="M20 12H15C14.4477 12 14 11.5523 14 11V7.5C14 6.94772 14.4477 6.5 15 6.5H19C19.5523 6.5 20 6.94772 20 7.5V12ZM20 12C20 14.5 19 16 16 17.5" stroke="var(--background-color)" stroke-width="1.5" stroke-linecap="round"></path></svg>
                                     {{-- <img src="{{ asset('assets/comma.png') }}" style="filter: hue-rotate(199deg);" alt=""> --}}
                                     {{-- <img src="{{ asset('assets/comma.png') }}" style="filter: hue-rotate(220deg);" alt=""> --}}
